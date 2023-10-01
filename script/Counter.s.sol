610cdb39610d942c407add4a160972f3720fd38b
ElahiAli <ali_elahi00@outlook.com>
2023-10-01 11:52:22 +0330

    initial foundry project

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
+import "@solmate-utils/LibString.sol";
+import "@openzeppelin/ERC721.sol";
 
 contract CounterScript is Script {
     function setUp() public {}
